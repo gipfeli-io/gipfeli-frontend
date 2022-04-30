91ae793b1e9b0de293a467e34cd560c35d9d3e76
Lukas Merz <lukemerz@gmail.com>
2022-04-30 09:34:23 +0200

    Add basic MUI setup, remove bloat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
