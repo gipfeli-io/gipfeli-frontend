dad154b4ca8467bff3570322fa72799ffda9d7ad
Lukas Merz <lukemerz@gmail.com>
2022-05-12 20:26:25 +0200

    Add basic setup

---
+++ __tests__/unit/services/auth-service.test.tsx
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom'
+import AuthService from '../../../src/services/auth/auth-service'
+
+const mockUser = 'test'
+const mockPassword = 'password'
+
+describe('AuthService', () => {
+    beforeEach(() => {
+        fetchMock.resetMocks()
+    })
+
+    it('has correct prefix set', () => {
+        expect(AuthService).toHaveProperty('prefix', 'auth')
+    })
+
+    it('calls API with correct login params', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}))
+
+        const login = await AuthService.login(mockUser, mockPassword)
+
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetchMock.mock.calls[0][1]?.body).toContain(mockUser)
+        expect(fetchMock.mock.calls[0][1]?.body).toContain(mockPassword)
+    })
+
+    it('calls API and returns the response as JSON', async () => {
+        const mockResponse = {data: 'thisIsTheMock'}
+        fetchMock.mockResponseOnce(JSON.stringify(mockResponse))
+
+        const login = await AuthService.login(mockUser, mockPassword)
+        const result = await login.json()
+
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(result).toEqual(mockResponse)
+    })
+})
