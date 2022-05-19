036a0da70f0a12780a6e1d3057162db62e92002c
Sabrina Wullschleger <sabrina.joerg@ost.ch>
2022-05-19 22:52:40 +0200

    add state usage to update values add dummy save functionality

--- src/components/app/TourForm.tsx
+++ src/components/app/TourForm.tsx
@@ -1,15 +1,43 @@
 import {Tour} from "../../types/tour";
 import {Button, Grid, TextField} from "@mui/material";
+import React, {ChangeEvent, useState} from "react";
+import {generators} from "openid-client";
+import {setState} from "jest-circus";
 
-export default function TourForm(props: { tour: Tour }) { //todo: add enum for form type
+type formProps = {
+    tour: Tour
+}
+
+export default function TourForm({tour}: formProps) { //todo: add enum for form type
+
+    const saveTour = async (event: any) => {
+        event.preventDefault();
+        console.log('saveTour:', currentTour);
+    }
+
+    const [currentTour, updateValue] = useState(tour);
+
     return <>
-        <form action="" method="post">
+        <form onSubmit={saveTour}>
             <Grid container spacing={4} direction={'row'} alignItems={'center'} justifyContent={'space-evenly'}>
                 <Grid item xs={12}>
-                    <TextField fullWidth variant="standard" label="Tour Name" value={props.tour.name}/>
+                    <TextField fullWidth
+                               variant="standard"
+                               label="Tour Name"
+                               value={currentTour.name}
+                               required
+                               onChange={(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) =>
+                                   updateValue({...currentTour, name: event.target.value})}
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField fullWidth multiline rows={5} label="Tour Description" value={props.tour.description}/>
+                    <TextField fullWidth
+                               multiline
+                               rows={5}
+                               label="Tour Description"
+                               value={currentTour.description}
+                               onChange={(event:ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) =>
+                                   updateValue({...currentTour, description: event.target.value})}/>
                 </Grid>
             </Grid>
             <Grid container spacing={2} mt={2} direction={'row'} alignItems={'center'} justifyContent={'center'}>
@@ -17,7 +45,7 @@ export default function TourForm(props: { tour: Tour }) { //todo: add enum for f
                     <Button fullWidth variant="outlined">Cancel</Button>
                 </Grid>
                 <Grid item xs={12} sm={3}>
-                    <Button fullWidth variant="contained" color="primary">Submit</Button>
+                    <Button type="submit" fullWidth variant="contained" color="primary">Submit</Button>
                 </Grid>
             </Grid>
         </form>
