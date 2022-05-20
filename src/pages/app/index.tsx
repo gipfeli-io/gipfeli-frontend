33703dd1aa394367d522228d701791d19a61f6ec
Lukas Merz <lukemerz@gmail.com>
2022-05-20 12:12:24 +0200

    Add working casts

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -5,82 +5,18 @@ import AppPageLayout from '../../layouts/app-page-layout'
 import {Tour} from '../../types/tour'
 import TourList from '../../components/app/TourList'
 import Typography from '@mui/material/Typography'
-import {plainToClass, plainToInstance} from 'class-transformer'
+import {plainToInstance} from 'class-transformer'
+import ToursService from '../../services/tours/tours-service'
 
 type AppHomeProps = {
     tours: Tour[]
 }
 
-const serviceResponse: Tour[] = [
-    {
-        'id': '7eb9cfff-d76f-4421-9064-586cc0511a30',
-        'name': 'test from nest, with user? yes but sabrinas tour now :) ',
-        'startLocation': {
-            'type': 'Point',
-            'coordinates': [
-                7.920462,
-                47.328439
-            ]
-        },
-        'endLocation': {
-            'type': 'Point',
-            'coordinates': [
-                47.328439,
-                7.920462
-            ]
-        },
-        'description': 'description from nest',
-        'createdAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
-        'updatedAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
-    },
-    {
-        'id': 'a815f336-1586-4857-a9cc-b521dac7d3c2',
-        'name': 'test from nest, with user? yes but sabrinas tour now :) ',
-        'startLocation': {
-            'type': 'Point',
-            'coordinates': [
-                7.920462,
-                47.328439
-            ]
-        },
-        'endLocation': {
-            'type': 'Point',
-            'coordinates': [
-                7.920462,
-                47.328439
-            ]
-        },
-        'description': 'description from nest',
-        'createdAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
-        'updatedAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
-    },
-    {
-        'id': '48d5f0d1-1f6a-4e63-8968-44f0718c979a',
-        'name': 'test from nest, with user? yes but sabrinas tour now :) ',
-        'startLocation': {
-            'type': 'Point',
-            'coordinates': [
-                7.920462,
-                48.328439
-            ]
-        },
-        'endLocation': {
-            'type': 'Point',
-            'coordinates': [
-                7.920462,
-                47.328439
-            ]
-        },
-        'description': 'description from nest',
-        'createdAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
-        'updatedAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
-    }
-]
 
 export const getServerSideProps = (context: NextPageContext) => withAuthenticatedOrRedirect(context, async (context: NextPageContext, session: Session) => {
-    const service = undefined // create TourService instance
-    const res = undefined // call TourService.getTours() property
-    const body: Tour[] = serviceResponse // call res.json()
+    const service = new ToursService(session)
+    const res = await service.mockAll()
+    const body: Tour[] = res // call res.json()
 
     return {
         props: {
@@ -89,15 +25,15 @@ export const getServerSideProps = (context: NextPageContext) => withAuthenticate
     }
 })
 
-const AppHome = ({tours}: AppHomeProps) => {
-    const a = plainToInstance(Tour, tours)
-    console.log(a)
+const AppHome = ({tours}: AppHomeProps): JSX.Element => {
+    tours = plainToInstance(Tour, tours) // todo: maybe have this in a generic fashion?
+
     return (
         <AppPageLayout>
             <Typography variant="h2" gutterBottom component="div">
                 My Tours
             </Typography>
-            <TourList rows={a}/>
+            <TourList rows={tours}/>
         </AppPageLayout>
     )
 }
---
+++ src/services/tours/tours-service.ts
@@ -0,0 +1,87 @@
+import APIService from '../api-service'
+import {Session} from 'next-auth'
+import {Tour} from '../../types/tour'
+
+const serviceResponse: Tour[] = [
+    {
+        'id': '7eb9cfff-d76f-4421-9064-586cc0511a30',
+        'name': 'test from nest, with user? yes but sabrinas tour now :) ',
+        'startLocation': {
+            'type': 'Point',
+            'coordinates': [
+                7.920462,
+                47.328439
+            ]
+        },
+        'endLocation': {
+            'type': 'Point',
+            'coordinates': [
+                47.328439,
+                7.920462
+            ]
+        },
+        'description': 'description from nest',
+        'createdAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
+        'updatedAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
+    },
+    {
+        'id': 'a815f336-1586-4857-a9cc-b521dac7d3c2',
+        'name': 'test from nest, with user? yes but sabrinas tour now :) ',
+        'startLocation': {
+            'type': 'Point',
+            'coordinates': [
+                7.920462,
+                47.328439
+            ]
+        },
+        'endLocation': {
+            'type': 'Point',
+            'coordinates': [
+                7.920462,
+                47.328439
+            ]
+        },
+        'description': 'description from nest',
+        'createdAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
+        'updatedAt': 'asd' as unknown as Date,
+    },
+    {
+        'id': '48d5f0d1-1f6a-4e63-8968-44f0718c979a',
+        'name': 'test from nest, with user? yes but sabrinas tour now :) ',
+        'startLocation': {
+            'type': 'Point',
+            'coordinates': [
+                7.920462,
+                48.328439
+            ]
+        },
+        'endLocation': {
+            'type': 'Point',
+            'coordinates': [
+                7.920462,
+                47.328439
+            ]
+        },
+        'description': 'description from nest',
+        'createdAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
+        'updatedAt': '2022-05-18T18:27:11.887Z' as unknown as Date,
+    }
+]
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export default class ToursService extends APIService {
+    private prefix: string = 'tours'
+
+    constructor(session: Session) {
+        super()
+        this.session = session
+    }
+
+    public async mockAll(): Promise<Tour[]> {
+        await sleep(150)
+        return serviceResponse
+    }
+}
