51f360840d4d9e7670c2fb7596b0394b77f43080
Lukas Merz <lukemerz@gmail.com>
2022-05-21 11:37:41 +0200

    Basic detail view

---
+++ src/pages/tours/[id].tsx
@@ -0,0 +1,59 @@
+import {NextPageContext} from 'next'
+import {withAuthenticatedOrRedirect} from '../../utils/with-authenticated-or-redirect'
+import {Session} from 'next-auth'
+import AppPageLayout from '../../layouts/app-page-layout'
+import {Tour} from '../../types/tour'
+import Typography from '@mui/material/Typography'
+import {plainToInstance} from 'class-transformer'
+import ToursService from '../../services/tours/tours-service'
+import {Button, Grid} from '@mui/material'
+
+type TourDetailProps = {
+    tour: Tour
+}
+
+
+export const getServerSideProps = (context: NextPageContext) => withAuthenticatedOrRedirect(context, async (context: NextPageContext, session: Session) => {
+    const service = new ToursService(session)
+    const res = await service.mockOne()
+    const body: Tour = res
+
+    return {
+        props: {
+            tour: body
+        }
+    }
+})
+
+const TourDetail = ({tour}: TourDetailProps): JSX.Element => {
+    tour = plainToInstance(Tour, tour) // todo: maybe have this in a generic fashion?
+    console.log(tour)
+
+    return (
+        <AppPageLayout>
+            <Typography variant="h2" gutterBottom component="div">
+                {tour.name}
+            </Typography>
+            <Grid container mb={2} direction={'row'} spacing={10}>
+                <Grid item>
+                    <Typography variant="subtitle1" gutterBottom component="div">
+                        Created at: {tour.getFormattedDate()}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant="subtitle1" gutterBottom component="div">
+                        Created at: {tour.getFormattedDate()}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant="subtitle1" gutterBottom component="div">
+                        Updated at: {tour.getFormattedDate()}
+                    </Typography>
+                </Grid>
+            </Grid>
+
+        </AppPageLayout>
+    )
+}
+
+export default TourDetail
--- src/types/tour.ts
+++ src/types/tour.ts
@@ -2,23 +2,17 @@ import {Point} from 'geojson'
 import {Type} from 'class-transformer'
 
 export class Tour {
-    id: string
-    name: string
-    startLocation: Point
-    endLocation: Point
-    description: string
+    id!: string
+    name!: string
+    startLocation!: Point
+    endLocation!: Point
+    description!: string
     @Type(() => Date)
-    createdAt: Date
+    createdAt!: Date
     @Type(() => Date)
-    updatedAt: Date // todo: make nullable as for create this will not be set
+    updatedAt!: Date // todo: make nullable as for create this will not be set
 
-    constructor(id: string, name: string, startLocation: Point, endLocation: Point, description: string, createdAt: Date, updatedAt: Date) {
-        this.id = id
-        this.name = name
-        this.startLocation = startLocation
-        this.endLocation = endLocation
-        this.description = description
-        this.createdAt = createdAt
-        this.updatedAt = updatedAt
+    public getFormattedCreatedAt(): string {
+        return this.createdAt.toLocaleDateString('de-CH') //todo: this should be set according to the users translation settings
     }
 }
