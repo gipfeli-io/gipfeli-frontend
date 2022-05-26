f0571a4859aa777e683ff3887b00b99361f84b31
Lukas Merz <lukemerz@gmail.com>
2022-05-26 10:15:20 +0200

    Make create endpoint private

--- src/pages/tours/create.tsx
+++ src/pages/tours/create.tsx
@@ -4,6 +4,17 @@ import TourForm from "../../components/app/TourForm";
 import {Tour} from "../../types/tour";
 import {Point} from 'geojson'
 import dayjs from 'dayjs'
+import {GetServerSideProps} from 'next'
+import {withAuthenticatedOrRedirect} from '../../utils/with-authenticated-or-redirect'
+import {Session} from 'next-auth'
+import ToursService from '../../services/tours/tours-service'
+
+export const getServerSideProps: GetServerSideProps = (context) => withAuthenticatedOrRedirect(context, async (context, session: Session) => {
+    return {
+        props: {
+        }
+    }
+})
 
 const NewTour = () => {
     const tour: Tour = new Tour("", "", {} as Point, {} as Point, "", dayjs(), dayjs())
