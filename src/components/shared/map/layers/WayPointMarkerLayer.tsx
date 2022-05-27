a3ba27d64f9f155b68c00fd5276ab71491b891ba
Sabrina Wullschleger <sabrina.joerg@ost.ch>
2022-05-27 15:45:59 +0200

    refactor marker layer

--- src/components/shared/map/layers/WayPointMarkerLayer.tsx
+++ src/components/shared/map/layers/WayPointMarkerLayer.tsx
@@ -28,62 +28,77 @@ const WayPointMarkerLayer = ({features, type, handleSetMarker}: WayPointMarkerLa
 
     useEffect(() => {
 
-        const mapConfigurationService = new MapConfigurationService()
-        const maxMarkerCount = mapConfigurationService.getMaxMarkerCount() //this might change in the future?
-        const startIcon = mapConfigurationService.getStartIcon()
-        const endIcon = mapConfigurationService.getEndIcon()
-        let layerExtent;
-        let markerLayer: VectorLayer<VectorSource>;
-
+        // if map or features are not set do nothing
         if (!map || !features) {
             return
         }
 
-        markerLayer = map.getAllLayers().find((layer: Layer) => layer.getProperties()['name'] === 'marker_layer') as VectorLayer<VectorSource>
-        if(!markerLayer) {
-            const {layer} = createMarkerLayer()
-            markerLayer = layer as VectorLayer<VectorSource>
-            layerExtent = addLayerFeatures(features, markerLayer as VectorLayer<VectorSource>)
-            map.addLayer(markerLayer)
-        } else {
-            markerLayer.getSource()?.clear(true)
-            layerExtent = addLayerFeatures(features, markerLayer as VectorLayer<VectorSource>)
+        const mapConfigurationService = new MapConfigurationService()
+        const maxMarkerCount = mapConfigurationService.getMaxMarkerCount()
+        const startIcon = mapConfigurationService.getStartIcon()
+        const endIcon = mapConfigurationService.getEndIcon()
+
+        const setupMarkerLayer = (): VectorLayer<VectorSource> => {
+            let markerLayer = map!.getAllLayers().find((layer: Layer) => layer.getProperties()['name'] === 'marker_layer') as VectorLayer<VectorSource>
+            let layerExtent: number[]
+
+            if(!markerLayer) {
+                const {layer} = createMarkerLayer()
+                markerLayer = layer as VectorLayer<VectorSource>
+                layerExtent = addLayerFeatures(features, markerLayer as VectorLayer<VectorSource>)
+                map!.addLayer(markerLayer)
+            } else {
+                markerLayer.getSource()?.clear(true)
+                layerExtent = addLayerFeatures(features, markerLayer as VectorLayer<VectorSource>)
+            }
+
+            //if no features were added the extent is set to an empty array
+            if(layerExtent!.length !== 0){
+                map!.getView().fit(layerExtent!, {size: map!.getSize(), padding: [100, 100, 100, 100]})
+            }
+
+            return markerLayer
         }
 
-        //if no features were added the extent is set to an empty array
-        if(layerExtent!.length !== 0){
-            map.getView().fit(layerExtent!, {size: map.getSize(), padding: [100, 100, 100, 100]})
+        const addModifyInteraction = (source: VectorSource): void => {
+            map!.addInteraction(new Modify({source: source}))
         }
 
+        const setupDrawInteraction = (source: VectorSource, markerId: number): void => {
+            const draw = new Draw({
+                source: source,
+                type: 'Point',
+                style: new Style({
+                    image: new Icon(({
+                        anchor: [0.5, 1],
+                        src: markerId === 0 ? startIcon : endIcon
+                    }))
+                })
+            })
+
+            map!.addInteraction(draw)
+
+            draw.on('drawend', (evt)=> {
+                const selectedFeature: Geometry = evt.feature.getGeometry()!
+                const point: Point = new Feature(selectedFeature.clone().transform('EPSG:3857', 'EPSG:4326')).getGeometry() as Point
+                handleSetMarker!(point.getCoordinates(), markerId)
+                markerId++
+                //remove draw interaction if we reach the max marker count
+                if(markerId >= maxMarkerCount){
+                    map!.removeInteraction(draw)
+                }
+            })
+        }
+
+        const layer = setupMarkerLayer()
+        const source = layer.getSource() as VectorSource
+
+        // only add interactions when type is set (type = 'edit' or 'create')
         if(type){
-            const source = markerLayer.getSource() as VectorSource
             let markerId = source.getFeatures().length
-            const modify = new Modify({source: source})
-            map.addInteraction(modify)
+            addModifyInteraction(source)
             if(type==='Create' && markerId < maxMarkerCount){
-                const draw = new Draw({
-                    source: markerLayer.getSource() as VectorSource,
-                    type: 'Point',
-                    style: new Style({
-                        image: new Icon(({
-                            anchor: [0.5, 1],
-                            src: markerId === 0 ? startIcon : endIcon
-                        }))
-                    })
-                })
-
-                map.addInteraction(draw)
-
-                draw.on('drawend', (evt)=> {
-                    const selectedFeature: Geometry = evt.feature.getGeometry()!
-                    const point: Point = new Feature(selectedFeature.clone().transform('EPSG:3857', 'EPSG:4326')).getGeometry() as Point
-                    handleSetMarker!(point.getCoordinates(), markerId)
-                    markerId++
-                    //remove draw interaction if we reach the max marker count
-                    if(markerId >= maxMarkerCount){
-                        map.removeInteraction(draw)
-                    }
-                })
+                setupDrawInteraction(source, markerId)
             }
         }
     }, [map, features, handleSetMarker, type])
