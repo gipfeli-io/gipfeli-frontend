ddc133aea0a2fcbafb8eb7b3de783b2433bbe41e
Lukas Merz <lukemerz@gmail.com>
2022-06-03 16:37:36 +0200

    Add edit functionality

---
+++ src/components/pages/tours/TourEdit.tsx
@@ -0,0 +1,48 @@
+import Typography from '@mui/material/Typography'
+import { Tour, UpdateOrCreateTour } from '../../../types/tour'
+import TourForm from '../../../components/app/TourForm'
+import ToursService from '../../../services/tours/tours-service'
+import { handleSave } from '../../../types/handle-save'
+import { useNavigate, useParams } from 'react-router'
+import useAuth from '../../../hooks/use-auth'
+import React, { useEffect, useState } from 'react'
+import { plainToInstance } from 'class-transformer'
+import Loader from '../../shared/Loader'
+
+const EditTour = () => {
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const auth = useAuth()
+  const [tour, setTour] = useState<UpdateOrCreateTour | undefined>(undefined)
+  const service = new ToursService(auth.token)
+
+  useEffect(() => {
+    async function fetchTour () {
+      const tour = await service.findOne(id!)
+      setTour(plainToInstance<Tour, Tour>(Tour, tour))
+    }
+
+    fetchTour()
+  }, [])
+
+  const handleSave: handleSave<UpdateOrCreateTour> = async (tour: UpdateOrCreateTour) => {
+    const result = await service.update(id!, tour) // todo: handle errors
+    if (result.ok) {
+      navigate('/tours')
+    }
+  }
+
+  if (!tour) {
+    return (<Loader />)
+  }
+  return (
+    <>
+      <Typography variant="h2" gutterBottom component="div">
+        Edit Tour
+      </Typography>
+      <TourForm tour={tour} handleSave={handleSave} type={'Edit'}/>
+    </>
+  )
+}
+
+export default EditTour
