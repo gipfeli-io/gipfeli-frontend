f3af39c2580cbb6c41f47f820d4ed4026b82cf3f
Sabrina Wullschleger <sabrina.joerg@ost.ch>
2022-06-05 13:09:47 +0200

    update snapshots fix user section tests

--- __tests__/unit/components/NavBarUserSection.test.tsx
+++ __tests__/unit/components/NavBarUserSection.test.tsx
@@ -3,14 +3,15 @@ import '@testing-library/jest-dom'
 import React from 'react'
 // @ts-ignore
 import renderer from 'react-test-renderer'
-import { render } from '@testing-library/react'
 import NavBarUserSection from '../../../src/components/shared/NavBarUserSection'
+import { MemoryRouter } from 'react-router'
+import { render } from '@testing-library/react'
 
 const mockSignIn: jest.Mock = jest.fn()
 const mockSignOut: jest.Mock = jest.fn()
-const mockUseContext = {
-  username: 'test@gipfeli.io',
-  token: 'mockedToken',
+const mockAuthenticationContext: any = {
+  username: null,
+  token: null,
   mockSignIn,
   mockSignOut
 }
@@ -22,53 +23,72 @@ const mockUseLocationValue = {
   state: null
 }
 
-jest.mock('../../../src/hooks/use-auth', () => jest.fn().mockImplementation(() => mockUseContext))
+const mockUsername = 'test@gipfeli.io'
+const mockToken = 'mockedToken'
+
+jest.mock('../../../src/hooks/use-auth', () => jest.fn().mockImplementation(() => mockAuthenticationContext))
 
 jest.mock('react-router', () => ({
   ...jest.requireActual('react-router') as any,
-  useNavigate: () => (jest.fn()),
-  useLocation: () => jest.fn().mockImplementation(() => mockUseLocationValue)
+  useNavigate: () => jest.fn(),
+  useLocation: () => jest.fn().mockImplementation(() => mockUseLocationValue),
+  useHref: () => jest.fn()
 }))
 
 describe('NavBarUserSection', () => {
-  it('behaves consistently when logged in', () => {
-    const tree = renderer
-      .create(<NavBarUserSection/>)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+  describe('test with logged in user', () => {
+    beforeEach(() => {
+      mockAuthenticationContext.token = mockToken
+      mockAuthenticationContext.username = mockUsername
+    })
+
+    it('behaves consistently when logged in', () => {
+      const tree = renderer
+        .create(<MemoryRouter initialEntries={['/currentUri']}><NavBarUserSection/></MemoryRouter>)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+    it('shows logout button and name of user if logged in', async () => {
+      mockAuthenticationContext.username = mockUsername
+      mockAuthenticationContext.token = mockToken
+      const { container, queryByText } = render(
+    <MemoryRouter initialEntries={['/currentUri']}><NavBarUserSection/></MemoryRouter>
+      )
+
+      const logoutButton = container.querySelector('#logout-button')
+      const joinButton = container.querySelector('#join-button')
+      // @ts-ignore
+      const greeting = queryByText('test@gipfeli.io', { exact: false })
+
+      expect(logoutButton).toBeInTheDocument()
+      expect(joinButton).not.toBeInTheDocument()
+      expect(greeting).toBeInTheDocument()
+    })
   })
 
-  it('behaves consistently when not logged in', () => {
-    const tree = renderer
-      .create(<NavBarUserSection/>)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+  describe('test with logged out user', () => {
+    beforeEach(() => {
+      mockAuthenticationContext.token = null
+      mockAuthenticationContext.username = null
+    })
 
-  it('shows login button if not logged in', async () => {
-    const { container } = render(
-            <NavBarUserSection/>
-    )
+    it('behaves consistently when not logged in', () => {
+      const tree = renderer
+        .create(<MemoryRouter initialEntries={['/currentUri']}><NavBarUserSection/></MemoryRouter>)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
 
-    const joinButton = container.querySelector('#join-button')
-    const logoutButton = container.querySelector('#logout-button')
+    it('shows login button if not logged in', async () => {
+      const { container } = render(
+        <MemoryRouter initialEntries={['/currentUri']}><NavBarUserSection/></MemoryRouter>
+      )
 
-    expect(joinButton).toBeInTheDocument()
-    expect(logoutButton).not.toBeInTheDocument()
-  })
+      const joinButton = container.querySelector('#join-button')
+      const logoutButton = container.querySelector('#logout-button')
 
-  it('shows logout button and name of user if logged in', async () => {
-    const { container, queryByText } = render(
-            <NavBarUserSection/>
-    )
-
-    const logoutButton = container.querySelector('#logout-button')
-    const joinButton = container.querySelector('#join-button')
-    // @ts-ignore
-    const greeting = queryByText('test@gipfeli.io', { exact: false })
-
-    expect(logoutButton).toBeInTheDocument()
-    expect(joinButton).not.toBeInTheDocument()
-    expect(greeting).toBeInTheDocument()
+      expect(joinButton).toBeInTheDocument()
+      expect(logoutButton).not.toBeInTheDocument()
+    })
   })
 })
