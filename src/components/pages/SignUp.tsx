d1fc24fb7a73a6b812bc5d1c13b2a453b14a45ed
Lukas Merz <lukemerz@gmail.com>
2022-06-14 20:48:22 +0200

    Typo

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -105,7 +105,7 @@ const SignUp = () => {
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
         >
-          Sign In
+          Sign Up
         </Button>
         <Grid container>
           <Grid item xs>
