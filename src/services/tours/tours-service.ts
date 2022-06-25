3ff4c6c7ea1bee816fb7cfa55ed4118a8074a64a
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-06-25 10:47:15 +0200

    Add handlers for handling offline tours

--- src/services/tours/tours-service.ts
+++ src/services/tours/tours-service.ts
@@ -2,6 +2,7 @@ import APIService from '../api-service'
 import { Tour, UpdateOrCreateTour } from '../../types/tour'
 import { ArrayApiResponse, SingleApiResponse } from '../../types/api'
 import LocalDatabaseService from '../local-database-service'
+import dayjs from 'dayjs'
 
 export default class ToursService extends APIService {
   private prefix: string = 'tours'
@@ -32,11 +33,12 @@ export default class ToursService extends APIService {
   }
 
   public async create (tour: UpdateOrCreateTour): Promise<SingleApiResponse<Tour>> {
-    return this.fetchSingleDataFromApi(
+    const result = await this.fetchSingleDataFromApi(
       this.getRequestUrl(this.prefix),
       this.getRequestBody('POST', tour),
       Tour
     )
+    return this.handleTourAddResult(tour, result)
   }
 
   public async update (id: string, tour: UpdateOrCreateTour): Promise<SingleApiResponse<void>> {
@@ -53,16 +55,37 @@ export default class ToursService extends APIService {
     )
   }
 
+  private async handleTourAddResult (tour: UpdateOrCreateTour, result: SingleApiResponse<Tour>): Promise<SingleApiResponse<Tour>> {
+    if (result.statusCode === 500) {
+      const wrapper = this.createResponseWrapper(true, 200, 'added data to local database')
+      const localTour = this.createLocalTour(tour)
+      await this.localDatabaseService.addTour(localTour)
+      return { content: localTour, ...wrapper }
+    } else if (result.statusCode === 201) {
+      await this.localDatabaseService.addTour(result.content!)
+    }
+
+    return result
+  }
+
+  private createLocalTour (tour: UpdateOrCreateTour): Tour {
+    const id = crypto.randomUUID().toString()
+    const localTour = new Tour(id, tour.name, tour.startLocation, tour.endLocation, tour.description, dayjs().toDate(), dayjs().toDate())
+    localTour.isSynced = false
+    return localTour
+  }
+
   private async handleTourListResult (result: ArrayApiResponse<Tour>): Promise<ArrayApiResponse<Tour>> {
     if (result.statusCode === 500) {
       const wrapper = this.createResponseWrapper(true, 200, 'serve data from local database')
       const tours = await this.localDatabaseService.findAllTours()
       return { content: tours, ...wrapper }
+    } else {
+      if (!result.content) {
+        result.content = []
+      }
+      await this.localDatabaseService.addTourList(result.content)
+      return result
     }
-    if (!result.content) {
-      result.content = []
-    }
-    await this.localDatabaseService.addTourList(result.content)
-    return result
   }
 }
