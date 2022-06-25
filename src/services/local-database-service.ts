de5a79602bcccc0c21dd9e0b4c0d855362e1a539
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-06-25 10:56:42 +0200

    Fix add tour list issue with deleting all entries Introduce add tour function

--- src/services/local-database-service.ts
+++ src/services/local-database-service.ts
@@ -3,11 +3,14 @@ import { localDB } from '../components/shared/local-database/local-db'
 
 export default class LocalDatabaseService {
   public async addTourList (tours: Tour[]): Promise<void> {
-    await localDB.tours.clear()
-    await localDB.tours.bulkAdd(tours)
+    await localDB.tours.bulkPut(tours)
   }
 
   public async findAllTours (): Promise<Tour[]> {
     return localDB.tours.toArray()
+  }
+
+  public async addTour (tour: Tour): Promise<void> {
+    await localDB.tours.add(tour)
   }
 }
