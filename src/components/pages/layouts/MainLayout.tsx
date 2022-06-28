6b6779495076554f01abe2d802fd8288f536b615
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-06-28 18:36:06 +0200

    Add offline notification component

--- src/components/pages/layouts/MainLayout.tsx
+++ src/components/pages/layouts/MainLayout.tsx
@@ -6,30 +6,38 @@ import Copyright from '../../shared/Copyright'
 import { Link, Outlet } from 'react-router-dom'
 import NavBarLinkSection from '../../shared/navbar/NavBarLinkSection'
 import ThemeSwitcher from '../../shared/navbar/ThemeSwitcher'
+import useOnlineStatus from '../../../hooks/use-online-status'
+import Banner from '../../shared/Banner'
+import OfflineNotification from '../../shared/OfflineNotification'
 
-const MainLayout = () => (
-  <>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          sx={{ mr: 2 }}
-          component={Link}
-          to='/'
-        >
-          <LandscapeIcon/>
-        </IconButton>
-        <NavBarLinkSection/>
-        <NavBarUserSection/>
-        <ThemeSwitcher/>
-      </Toolbar>
-    </AppBar>
+const MainLayout = () => {
+  const isOnline = useOnlineStatus()
 
-    <Outlet/>
+  const offlineBannerContent = isOnline ? null : <OfflineNotification/>
 
-    <Copyright/>
-  </>
-)
+  return (<>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            sx={{ mr: 2 }}
+            component={Link}
+            to='/'
+          >
+            <LandscapeIcon/>
+          </IconButton>
+          <NavBarLinkSection/>
+          <NavBarUserSection/>
+          <ThemeSwitcher/>
+        </Toolbar>
+      </AppBar>
+      <Banner bannerContent={offlineBannerContent}/>
+      <Outlet/>
+
+      <Copyright/>
+    </>
+  )
+}
 
 export default MainLayout
