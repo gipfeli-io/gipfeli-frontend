5a5d731355cad2ce1675b454960adedf55b42882
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-06-30 19:22:13 +0200

    Fix online status and remove navigator.Online because it is utterly useless

--- src/components/providers/OnlineStatusProvider.tsx
+++ src/components/providers/OnlineStatusProvider.tsx
@@ -1,18 +1,26 @@
-import React, { PropsWithChildren, useEffect, useState } from 'react'
+import React, { PropsWithChildren, useState } from 'react'
 import OnlineStatusContext from '../../contexts/online-status-context'
+import useInterval from '../../hooks/use-interval'
 
 export const OnlineStatusProvider = ({ children }: PropsWithChildren<any>) => {
   const [onlineStatus, setOnlineStatus] = useState<boolean>(true)
+  const requestUrl: string = process.env.REACT_APP_PUBLIC_BACKEND_API || 'http://localhost:3000'
+  const pollingDelay: string | number = process.env.ONLINE_POLLING_DELAY || 60000
 
-  useEffect(() => {
-    window.addEventListener('offline', () => setOnlineStatus(false))
-    window.addEventListener('online', () => setOnlineStatus(true))
-
-    return () => {
-      window.removeEventListener('offline', () => setOnlineStatus(false))
-      window.removeEventListener('online', () => setOnlineStatus(true))
+  const checkOnlineStatus = async (): Promise<void> => {
+    try {
+      const request = await fetch(requestUrl + '/robots.txt')
+      if (request.status < 500) {
+        setOnlineStatus(true)
+      } else {
+        setOnlineStatus(false)
+      }
+    } catch (error) {
+      setOnlineStatus(false)
     }
-  }, [])
+  }
+
+  useInterval(checkOnlineStatus, +pollingDelay)
 
   return (
     <OnlineStatusContext.Provider value={onlineStatus}>
