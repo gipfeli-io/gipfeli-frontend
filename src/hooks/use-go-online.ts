9c63da05a6030e69d809af75b6e2a32cb318cc5b
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-07-31 15:34:18 +0200

    add try catch

--- src/hooks/use-go-online.ts
+++ src/hooks/use-go-online.ts
@@ -5,27 +5,33 @@ import useNotifications from './use-notifications'
 import ToursSyncService from '../services/tours/tours-sync-service'
 import useConnectionStatus from './use-connection-status'
 import { useCallback } from 'react'
+import useErrorHandling from './use-error-handling'
 
 const useGoOnline = () => {
   const auth = useAuth()
   const { triggerSyncFailedNotification } = useNotifications()
   const tourSyncService: ToursSyncService = new ToursSyncService(auth.token)
   const { updateConnectionStatus, updateOnlineInfoBannerVisibility } = useConnectionStatus()
+  const { triggerError } = useErrorHandling()
 
   return useCallback(() => {
     async function syncData () {
-      const results = await tourSyncService.synchronizeTourData()
-      results.forEach((result: SingleApiResponse<unknown>) => {
-        if (result.error) {
-          const { message } = result.error
+      try {
+        const results = await tourSyncService.synchronizeTourData()
+        results.forEach((result: SingleApiResponse<unknown>) => {
+          if (result.error) {
+            const { message } = result.error
 
-          if (message !== undefined) {
-            Array.isArray(message) ? triggerSyncFailedNotification('An error occurred') : triggerSyncFailedNotification(message)
+            if (message !== undefined) {
+              Array.isArray(message) ? triggerSyncFailedNotification('An error occurred') : triggerSyncFailedNotification(message)
+            }
           }
-        }
-      })
-      updateOnlineInfoBannerVisibility(false)
-      updateConnectionStatus(ConnectionStatus.ONLINE)
+        })
+        updateOnlineInfoBannerVisibility(false)
+        updateConnectionStatus(ConnectionStatus.ONLINE)
+      } catch (error: unknown) {
+        triggerError(error as Error)
+      }
     }
 
     syncData()
