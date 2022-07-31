2955053c8556704e8df7008b338bb1baa25016f4
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-07-31 17:10:01 +0200

    remove navigation from connection status provider as it interfered with displaying error messages

--- src/components/providers/ConnectionStatusProvider.tsx
+++ src/components/providers/ConnectionStatusProvider.tsx
@@ -5,13 +5,10 @@ import { ConnectionStatusContextType } from '../../types/contexts'
 import { LocalStorageKey } from '../../enums/local-storage-key'
 import LocalStorageService from '../../services/local-storage-service'
 import HeartbeatService from '../../services/heartbeat-service'
-import { useLocation, useNavigate } from 'react-router'
 
 export const ConnectionStatusProvider = ({ children }: PropsWithChildren<any>) => {
   const localStorageService = new LocalStorageService()
   const heartBeatService = new HeartbeatService()
-  const navigate = useNavigate()
-  const location = useLocation()
   const pollingDelay: string = process.env.REACT_APP_ONLINE_POLLING_DELAY || '20000'
   // eslint-disable-next-line no-undef
   let intervalId: NodeJS.Timer
@@ -87,11 +84,6 @@ export const ConnectionStatusProvider = ({ children }: PropsWithChildren<any>) =
       initPolling()
     } else {
       updateGoOnlineButtonVisibility(false)
-    }
-    if (location.pathname !== '/tours/create') {
-      navigate(0)
-    } else {
-      navigate('/tours', { replace: true })
     }
   }
 
