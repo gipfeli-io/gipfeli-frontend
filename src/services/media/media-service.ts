b692730abed56d9eed3650dffee912c14d95ef05
Lukas Merz <lukemerz@gmail.com>
2022-08-22 16:58:44 +0200

    Rename f ilename upload

--- src/services/media/media-service.ts
+++ src/services/media/media-service.ts
@@ -4,6 +4,7 @@ import { GpxFileUpload, ImageUpload } from '../../types/media'
 
 export default class MediaService extends APIService {
   private prefix: string = 'media'
+  private fileField: string = 'file'
 
   constructor (token: string | undefined) {
     super()
@@ -13,7 +14,7 @@ export default class MediaService extends APIService {
   public async uploadImage (image: File): Promise<SingleApiResponse<ImageUpload>> {
     return this.fetchSingleDataFromApi(
       this.getRequestUrl(this.prefix, 'upload-image'),
-      this.getRequestBody('POST', image, 'image'),
+      this.getRequestBody('POST', image),
       ImageUpload
     )
   }
@@ -21,17 +22,17 @@ export default class MediaService extends APIService {
   public async uploadGpxFile (gpxFile: File): Promise<SingleApiResponse<GpxFileUpload>> {
     return this.fetchSingleDataFromApi(
       this.getRequestUrl(this.prefix, 'upload-gpx-file'),
-      this.getRequestBody('POST', gpxFile, 'gpxFile'),
+      this.getRequestBody('POST', gpxFile),
       GpxFileUpload
     )
   }
 
-  protected getRequestBody (method: 'POST' | 'DELETE', file: File, fieldName: string): RequestBody {
+  protected getRequestBody (method: 'POST' | 'DELETE', file: File): RequestBody {
     // Get the auth token header from the parent method, we'll override the content here for handling file uploads
     const fileUploadBody = super.getRequestBody(method)
 
     const content = new FormData()
-    content.append(fieldName, file)
+    content.append(this.fileField, file)
     // Unset content-type to force fetch to set the content-type according to the uploaded file
     fileUploadBody.headers = { Authorization: fileUploadBody.headers.Authorization }
     fileUploadBody.body = content
