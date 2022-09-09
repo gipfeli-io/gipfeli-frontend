00a6ccdf54697075ad69141a2794168cb3bad556
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-09-09 12:03:48 +0200

    add jest setup for injecting reflect-metadata

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,8 @@ const customJestConfig = {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     // required to use import file (e.g. image) syntax
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
-  }
+  },
+  setupFiles: ['./jest-setup-file.ts']
 }
 
 module.exports = customJestConfig
