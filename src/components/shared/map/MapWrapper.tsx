0bb71398e04002436ed344790981ffc62a12c5ac
Lukas Merz <lukemerz@gmail.com>
2022-09-12 16:12:31 +0200

    Fix missing DOM id for ol-ext

--- src/components/shared/map/MapWrapper.tsx
+++ src/components/shared/map/MapWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useEffect, useId, useState } from 'react'
+import React, { PropsWithChildren, useId, useLayoutEffect, useState } from 'react'
 import { Map, View } from 'ol'
 import 'ol/ol.css'
 import TileLayer from 'ol/layer/Tile'
@@ -14,7 +14,17 @@ const MapWrapper = ({ children }: PropsWithChildren<any>) => {
   const mapContainerId = useId()
   const [map, setMap] = useState<Map | undefined>(undefined)
 
-  useEffect(() => {
+  /**
+   * Notice that we use useLayoutEffect() instead of useEffect(). The reason for this is that when we call dispose() on
+   * the map object while a user is hovering over an instance of ol-ext's Hover interaction (e.g. GPX track), so e.g.
+   * when the user hovers and then hits the return button; in that moment ol-ext needs to have the DOM present to get
+   * the default cursor (see https://github.com/Viglino/ol-ext/blob/master/src/interaction/Hover.js#L66). When we use
+   * useEffect(), ol-ext calls getTargetElement() on the map object, but at that point, the target element is already
+   * removed from the DOM.
+   *
+   * Note: It would actually be better if ol-ext would not be DOM-dependent, i.e. falling back to a default cursor.
+   */
+  useLayoutEffect(() => {
     const initialMap = new Map({
       target: mapContainerId,
       layers: [
